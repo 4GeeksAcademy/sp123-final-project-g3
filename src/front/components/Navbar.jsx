f8eac1f41820825598befa35ecb786fc8e764a02
Pimple <araozlucas@gmail.com>
2026-02-02 19:43:44 +0000

    Postulacion

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,12 +1,24 @@
 
 import { NavLink, Link } from "react-router-dom";
+import { useState } from "react";
 import "../index.css";
 import logo from "../imagenes/logo.png";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
+import Postulacion from "./Postulacion.jsx";
 
 export default function Navbar() {
-	return (
+	const [isPostulacionOpen, setIsPostulacionOpen] = useState(false);
+	
+	const handleOpen = () => setIsPostulacionOpen(true);
+	const handleClose = () => setIsPostulacionOpen(false);
+	
+	const handleCreate = (data) => {
+    console.log("Nueva postulación:", data);
+    setIsPostulacionOpen(false);
+	};
+	
+return (
 		<nav className="top-navbar">
 
 			{/* FILA SUPERIOR */}
@@ -25,7 +37,7 @@ export default function Navbar() {
 				</div>
 
 				{/* DERECHA: BOTÓN */}
-				<button className="btn-new-postulation">
+				<button className="btn-new-postulation" onClick={handleOpen} type="button">
 					<span className="btn-plus">+</span>
 					Nueva Postulación
 				</button>
@@ -34,7 +46,6 @@ export default function Navbar() {
 			{/* MENÚ */}
 			<ul className="navbar-menu">
 				<li>
-
 					<Link to="/" className="nav-item">
 						<i className="bi bi-layers me-2"></i>
 						Tablero Kanban
@@ -43,21 +54,18 @@ export default function Navbar() {
 				<li>
 					<NavLink to="/estadisticas" className="nav-item">
 						<i className="bi bi-bar-chart me-2"></i>
-
 						Estadísticas
 					</NavLink>
 				</li>
 				<li>
 					<NavLink to="/perfil" className="nav-item">
 						<i className="bi bi-person me-2"></i>
-
 						Mi Perfil
 					</NavLink>
 				</li>
 				<li>
 					<NavLink to="/buscar" className="nav-item">
 						<i className="bi bi-search me-2"></i>
-
 						Buscar Postulaciones
 					</NavLink>
 				</li>
