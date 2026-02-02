576859c8406ce59cb341e7a8c89c5da701d23e1d
Pimple <araozlucas@gmail.com>
2026-02-02 19:50:44 +0000

    Postulacion

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -19,68 +19,74 @@ export default function Navbar() {
 	};
 	
 return (
-		<nav className="top-navbar">
+		<>	
+			<nav className="top-navbar">
+				{/* FILA SUPERIOR */}
+				<div className="navbar-top-row">
+					
+					{/* IZQUIERDA: LOGO + TEXTO */}
+					<div className="navbar-left">
+						<div className="login-logo-form d-flex align-items-center mb-3">
+							<img src={logo} alt="logo" className="logo-image" />
+						</div>
 
-			{/* FILA SUPERIOR */}
-			<div className="navbar-top-row">
-
-				{/* IZQUIERDA: LOGO + TEXTO */}
-				<div className="navbar-left">
-					<div className="login-logo-form d-flex align-items-center mb-3">
-						<img src={logo} alt="logo" className="logo-image" />
+						<div className="navbar-text">
+							<h1>¡Futuro nombre super chulo!</h1>
+							<span>Gestiona tus postulaciones de empleo</span>
+						</div>
 					</div>
 
-					<div className="navbar-text">
-						<h1>¡Futuro nombre super chulo!</h1>
-						<span>Gestiona tus postulaciones de empleo</span>
-					</div>
+					{/* DERECHA: BOTÓN */}
+					<button className="btn-new-postulation" onClick={handleOpen} type="button">
+						<span className="btn-plus">+</span>
+						Nueva Postulación
+					</button>
 				</div>
 
-				{/* DERECHA: BOTÓN */}
-				<button className="btn-new-postulation" onClick={handleOpen} type="button">
-					<span className="btn-plus">+</span>
-					Nueva Postulación
-				</button>
-			</div>
+				{/* MENÚ */}
+				<ul className="navbar-menu">
+					<li>
+						<Link to="/" className="nav-item">
+							<i className="bi bi-layers me-2"></i>
+							Tablero Kanban
+						</Link>
+					</li>
+					<li>
+						<NavLink to="/estadisticas" className="nav-item">
+							<i className="bi bi-bar-chart me-2"></i>
+							Estadísticas
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/perfil" className="nav-item">
+							<i className="bi bi-person me-2"></i>
+							Mi Perfil
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/buscar" className="nav-item">
+							<i className="bi bi-search me-2"></i>
+							Buscar Postulaciones
+						</NavLink>
+					</li>
+					<li>
+						<Link to="/login" className="nav-item">
+							Login
+						</Link>
+					</li>
+					<li>
+						<Link to="/registro" className="nav-item">
+							Registro
+						</Link>
+					</li>
+				</ul>
 
-			{/* MENÚ */}
-			<ul className="navbar-menu">
-				<li>
-					<Link to="/" className="nav-item">
-						<i className="bi bi-layers me-2"></i>
-						Tablero Kanban
-					</Link>
-				</li>
-				<li>
-					<NavLink to="/estadisticas" className="nav-item">
-						<i className="bi bi-bar-chart me-2"></i>
-						Estadísticas
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to="/perfil" className="nav-item">
-						<i className="bi bi-person me-2"></i>
-						Mi Perfil
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to="/buscar" className="nav-item">
-						<i className="bi bi-search me-2"></i>
-						Buscar Postulaciones
-					</NavLink>
-				</li>
-				<li>
-					<Link to="/login" className="nav-item">
-						Login
-					</Link>
-				</li>
-				<li>
-					<Link to="/registro" className="nav-item">
-						Registro
-					</Link>
-				</li>
-			</ul>
-
-		</nav>
+			</nav>
+			<Postulacion
+			isOpen={isPostulacionOpen}
+			onClose={handleClose}
+			onCreate={handleCreate}
+			/>
+		</>
 	);
 }
